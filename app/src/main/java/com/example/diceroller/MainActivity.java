691512d89e0b9bf5b48212c9eebf1938adc9fa9f
Sajedul Islam Fahim <sajedulislamfahim@gmail.com>
2020-09-28 11:19:52 +0600

    Dice roller added

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changePic(View view) {
+        ImageView imageView=findViewById(R.id.imageView);
+        Random random=new Random();
+        int guess = random.nextInt(6)+1;
+        switch (guess){
+            case 1:
+                imageView.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                imageView.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                imageView.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                imageView.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                imageView.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                imageView.setImageResource(R.drawable.dice6);
+                break;
+
+        }
+    }
+}
